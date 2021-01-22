25f201455f60b56a7da4d6dec1e8e49ba1d73cb2
Ahnaf Sayem <sayem.ruet18@gmail.com>
2021-01-22 16:11:18 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,8 @@
+//Kilometer to Meter Conversion
+
+function kilometerToMeter(number){
+    var meter=number/1000;
+    return meter;
+}
+var result = kilometerToMeter(20);
+console.log("In Meter :", result)
